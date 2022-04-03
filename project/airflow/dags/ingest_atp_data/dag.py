f6525076496b7a991184eebb6f1fd7fff8b9410e
Igor Smirnov <igor.smirnov@crayon.com>
2022-04-03 19:10:19 +0200

    updated airflow scripts

--- project/airflow/dags/ingest_atp_data/dag.py
+++ project/airflow/dags/ingest_atp_data/dag.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator, BigQueryInsertJobOperator
 
-from scripts import upload_to_gcs
+from scripts import upload_to_gcs, csv_schemas
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
@@ -23,6 +23,12 @@ dataset_url_prefix = "https://raw.githubusercontent.com/JeffSackmann/tennis_atp/
 ds_years = ['70s', '80s', '90s', '00s', '10s', '20s', 'current']
 dataset_url_ranking = [dataset_url_prefix.format(ds_year=ds_year) for ds_year in ds_years]
 dataset_url_players = ["https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_players.csv"]
+
+data_dict = {
+    'atp_matches': dataset_url_matches,
+    'atp_rankings': dataset_url_ranking,
+    'atp_players': dataset_url_players
+}
 
 default_args = {
     "owner": "igor.l.smirnov@gmail.com",
@@ -43,50 +49,36 @@ with DAG(
     tags=['dtc-de-project'],
 ) as dag:
 
-    for dataset_url in dataset_url_players + dataset_url_ranking + dataset_url_matches:
-        dataset_file = os.path.split(dataset_url)[1]
-        dataset_name = dataset_file.split('.')[0]
-
-        download_dataset_task = BashOperator(
-            task_id=f"download_{dataset_name}_task",
-            bash_command=f"curl -sSf {dataset_url} > {path_to_local_home}/{dataset_file}"
-        )
-
-        local_to_gcs_task = PythonOperator(
-            task_id=f"local_to_gcs_{dataset_name}_task",
-            python_callable=upload_to_gcs,
-            op_kwargs={
-                "bucket": BUCKET,
-                "object_name": f"raw/{dataset_file}",
-                "local_file": f"{path_to_local_home}/{dataset_file}",
-            },
-        )
-
+    for data_type in data_dict:
         bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-            task_id=f"bq_ext_table_{dataset_name}_task",
+            task_id=f"bq_ext_table_{data_type}_task",
             table_resource={
                 "tableReference": {
                     "projectId": PROJECT_ID,
                     "datasetId": BIGQUERY_DATASET_EXT,
-                    "tableId": dataset_name,
+                    "tableId": data_type,
                 },
                 "externalDataConfiguration": {
                     "sourceFormat": "CSV",
-                    "autodetect": True,
-                    "allowJaggedRows": True,
-                    "sourceUris": [f"gs://{BUCKET}/raw/{dataset_file}"],
+                    "schema": {"fields": csv_schemas[data_type]},
+                    "sourceUris": [f"gs://{BUCKET}/raw/{data_type}/*.csv"],
+                    "maxBadRecords":1000,
+                    "csvOptions": {
+                        "skipLeadingRows": 1,
+                        "allowJaggedRows": False
+                    }
                 },
             },
         )
 
         CREATE_BQ_TBL_QUERY = f"""
-        CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{dataset_name}
+        CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{data_type}
         AS
-        SELECT * FROM {BIGQUERY_DATASET_EXT}.{dataset_name};
+        SELECT * FROM {BIGQUERY_DATASET_EXT}.{data_type};
         """
         
         create_table_task = BigQueryInsertJobOperator(
-            task_id=f"bq_table_{dataset_name}_task",
+            task_id=f"bq_table_{data_type}_task",
             configuration={
                 "query": {
                     "query": CREATE_BQ_TBL_QUERY,
@@ -94,5 +86,24 @@ with DAG(
                 }
             }
         )
+        
+        for dataset_url in data_dict[data_type]:
+            dataset_file = os.path.split(dataset_url)[1]
+            dataset_name = dataset_file.split('.')[0]
 
-        download_dataset_task >> local_to_gcs_task >> bigquery_external_table_task >> create_table_task
+            download_dataset_task = BashOperator(
+                task_id=f"download_{dataset_name}_task",
+                bash_command=f"curl -sSf {dataset_url} > {path_to_local_home}/{dataset_file}"
+            )
+
+            local_to_gcs_task = PythonOperator(
+                task_id=f"local_to_gcs_{dataset_name}_task",
+                python_callable=upload_to_gcs,
+                op_kwargs={
+                    "bucket": BUCKET,
+                    "object_name": f"raw/{data_type}/{dataset_file}",
+                    "local_file": f"{path_to_local_home}/{dataset_file}",
+                },
+            )
+
+            download_dataset_task >> local_to_gcs_task >> bigquery_external_table_task >> create_table_task
--- project/airflow/dags/scripts.py
+++ project/airflow/dags/scripts.py
@@ -20,3 +20,256 @@ def upload_to_gcs(bucket, object_name, local_file):
 
     blob = bucket.blob(object_name)
     blob.upload_from_filename(local_file)
+
+csv_schemas = {
+    'atp_matches': [
+        {
+            "name": "tourney_id",
+            "type": "STRING"
+        },
+        {
+            "name": "tourney_name",
+            "type": "STRING"
+        },
+        {
+            "name": "surface",
+            "type": "STRING"
+        },
+        {
+            "name": "draw_size",
+            "type": "INT64"
+        },
+        {
+            "name": "tourney_level",
+            "type": "STRING"
+        },
+        {
+            "name": "tourney_date",
+            "type": "INT64"
+        },
+        {
+            "name": "match_num",
+            "type": "INT64"
+        },
+        {
+            "name": "winner_id",
+            "type": "INT64"
+        },
+        {
+            "name": "winner_seed",
+            "type": "INT64"
+        },
+        {
+            "name": "winner_entry",
+            "type": "STRING"
+        },
+        {
+            "name": "winner_name",
+            "type": "STRING"
+        },
+        {
+            "name": "winner_hand",
+            "type": "STRING"
+        },
+        {
+            "name": "winner_ht",
+            "type": "INT64"
+        },
+        {
+            "name": "winner_ioc",
+            "type": "STRING"
+        },
+        {
+            "name": "winner_age",
+            "type": "FLOAT64"
+        },
+        {
+            "name": "loser_id",
+            "type": "INT64"
+        },
+        {
+            "name": "loser_seed",
+            "type": "INT64"
+        },
+        {
+            "name": "loser_entry",
+            "type": "STRING"
+        },
+        {
+            "name": "loser_name",
+            "type": "STRING"
+        },
+        {
+            "name": "loser_hand",
+            "type": "STRING"
+        },
+        {
+            "name": "loser_ht",
+            "type": "INT64"
+        },
+        {
+            "name": "loser_ioc",
+            "type": "STRING"
+        },
+        {
+            "name": "loser_age",
+            "type": "FLOAT64"
+        },
+        {
+            "name": "score",
+            "type": "STRING"
+        },
+        {
+            "name": "best_of",
+            "type": "INT64"
+        },
+        {
+            "name": "round",
+            "type": "STRING"
+        },
+        {
+            "name": "minutes",
+            "type": "INT64"
+        },
+        {
+            "name": "w_ace",
+            "type": "INT64"
+        },
+        {
+            "name": "w_df",
+            "type": "INT64"
+        },
+        {
+            "name": "w_svpt",
+            "type": "INT64"
+        },
+        {
+            "name": "w_1stIn",
+            "type": "INT64"
+        },
+        {
+            "name": "w_1stWon",
+            "type": "INT64"
+        },
+        {
+            "name": "w_2ndWon",
+            "type": "INT64"
+        },
+        {
+            "name": "w_SvGms",
+            "type": "INT64"
+        },
+        {
+            "name": "w_bpSaved",
+            "type": "INT64"
+        },
+        {
+            "name": "w_bpFaced",
+            "type": "INT64"
+        },
+        {
+            "name": "l_ace",
+            "type": "INT64"
+        },
+        {
+            "name": "l_df",
+            "type": "INT64"
+        },
+        {
+            "name": "l_svpt",
+            "type": "INT64"
+        },
+        {
+            "name": "l_1stIn",
+            "type": "INT64"
+        },
+        {
+            "name": "l_1stWon",
+            "type": "INT64"
+        },
+        {
+            "name": "l_2ndWon",
+            "type": "INT64"
+        },
+        {
+            "name": "l_SvGms",
+            "type": "INT64"
+        },
+        {
+            "name": "l_bpSaved",
+            "type": "INT64"
+        },
+        {
+            "name": "l_bpFaced",
+            "type": "INT64"
+        },
+        {
+            "name": "winner_rank",
+            "type": "INT64"
+        },
+        {
+            "name": "winner_rank_points",
+            "type": "INT64"
+        },
+        {
+            "name": "loser_rank",
+            "type": "INT64"
+        },
+        {
+            "name": "loser_rank_points",
+            "type": "INT64"
+        }
+    ],
+    'atp_rankings': [
+        {
+            "name": "ranking_date",
+            "type": "INT64"
+        },
+        {
+            "name": "rank",
+            "type": "INT64"
+        },
+        {
+            "name": "player",
+            "type": "INT64"
+        },
+        {
+            "name": "points",
+            "type": "INT64"
+        }
+    ],
+    'atp_players': [
+        {
+            "name": "player_id",
+            "type": "INT64"
+        },
+        {
+            "name": "name_first",
+            "type": "STRING"
+        },
+        {
+            "name": "name_last",
+            "type": "STRING"
+        },
+        {
+            "name": "hand",
+            "type": "STRING"
+        },
+        {
+            "name": "dob",
+            "type": "INT64"
+        },
+        {
+            "name": "ioc",
+            "type": "STRING"
+        },
+        {
+            "name": "height",
+            "type": "INT64"
+        },
+        {
+            "name": "wikidata_id",
+            "type": "STRING"
+        }
+    ]
+}
