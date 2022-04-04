9f215bb0dffc344ff21c2040f20e49c6d9b8ca0a
Igor Smirnov <igor.l.smirnov@gmail.com>
2022-04-04 13:00:40 +0000

    limit tourney_date to avoid old tournaments in current majors

--- project/dbt/models/core/current_majors.sql
+++ project/dbt/models/core/current_majors.sql
@@ -6,7 +6,8 @@ WITH t AS (
         t2.*, RANK() OVER(PARTITION BY t1.ranking_date, t2.tourney_name ORDER BY t2.tourney_date DESC) AS rn
     FROM {{ ref('stg_ranking_dates') }} t1
     LEFT JOIN {{ ref('stg_major_tourneys') }} t2
-        ON t2.tourney_date < t1.ranking_date
+        -- limit of 1000 days not to show very old tournaments
+        ON t2.tourney_date BETWEEN t1.ranking_date - INTERVAL 1000 DAY AND t1.ranking_date
 )
 SELECT
     ranking_date,
